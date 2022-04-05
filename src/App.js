326a78a492bd8190e9af9e7a0ee199c560459a5b
smedberg <smedberg@gmail.com>
2022-04-05 13:24:43 -0700

    Oops, misspelled "Grammar"- doh!

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import React from 'react';
 import './App.css';
 import FormulaInput from './FormulaInput';
 import ResultDisplay from './ResultDisplay';
-import parse from './SciGrammer'
+import { parse as SciParse } from './SciGrammar'
+
+// A global used in SciGrammar.pegjs.  We can't pass in
+// arguments, but we share a global environment.
+// We can also dynamically modify this map, e.g.
+// by adding new constants at runtime.
+window.SCIPARSER_CONSTANTS =  new Map([
+  ["C", [2.99792e8, "M/S"]],
+  ["Pi", [3.14159, "untyped"]],
+  ["E", [1.60218e-19, "C"]]
+]);
 
 class App extends React.Component {
   constructor() {
@@ -15,7 +25,10 @@ class App extends React.Component {
   render() {
     var resultText, units;
     try {
-      const parsed = parse.parse(this.state.formula);
+      // TODO: run each line separately.  If a line sets a constant, add it 
+      // to window.SCIPARSER_CONSTANTS like this:
+      // window.SCIPARSER_CONSTANTS.set("F", [3, "m/s"]);
+      const parsed = SciParse(this.state.formula);
       resultText = parsed[0];
       units = parsed[1];
     } catch(error) {
