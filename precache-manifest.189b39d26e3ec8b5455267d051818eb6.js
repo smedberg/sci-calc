cf14a5ebc78aef4fe238b8b34abd3d79672a1014
smedberg <smedberg@gmail.com>
2022-04-05 11:30:22 -0700

    Updates

---
+++ precache-manifest.189b39d26e3ec8b5455267d051818eb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a1d583f6227e49e9893",
+    "url": "/sci-calc/static/css/main.1504e184.chunk.css"
+  },
+  {
+    "revision": "8a1d583f6227e49e9893",
+    "url": "/sci-calc/static/js/main.8a1d583f.chunk.js"
+  },
+  {
+    "revision": "05a8e767b6b683ee5dc9",
+    "url": "/sci-calc/static/js/1.05a8e767.chunk.js"
+  },
+  {
+    "revision": "05ef9f1ce603c5674029",
+    "url": "/sci-calc/static/js/2.05ef9f1c.chunk.js"
+  },
+  {
+    "revision": "42362e0dc70172c7d695",
+    "url": "/sci-calc/static/js/runtime~main.42362e0d.js"
+  },
+  {
+    "revision": "0472f829f68716e1b6f6cb39a9533e0d",
+    "url": "/sci-calc/index.html"
+  }
+];
