635ed4b2560215fd770a394a1f76b0900ece7e1b
smedberg <smedberg@gmail.com>
2022-04-17 13:30:37 -0700

    Breaking out variables for display

--- src/ConstantsDisplay.test.js
+++ src/ConstantsDisplay.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import ConstantsDisplay from './ConstantsDisplay';
-import {SCIPARSER_CONSTANTS} from './Calculator';
+import {SCIPARSER_CONSTANTS_MAP} from './Calculator';
 
 test('renders constants area', () => {
-  render(<ConstantsDisplay constants={SCIPARSER_CONSTANTS} />);
+  const constantsMap = new Map([
+    ["Pi", [Math.PI, "untyped"], "Pi"],
+    ["E", [Math.E, "untyped"], "Euler's Constant"],
+    ["c", [2.99792e8, "m/s"], "Speed of Light"]
+  ]);
+  render(<ConstantsDisplay constants={constantsMap} />);
   const constantArea = screen.getByTestId("constants-display-area");
   expect(constantArea).toBeInTheDocument();
 });
